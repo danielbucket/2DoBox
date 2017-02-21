f63713968efa6f931399da2e08b70c1c267f1492
Daniel Bucket <danieljbucket@gmail.com>
2017-02-21 01:16:10 -0700

    a few more minor synatax correction to be made.

--- scripts.js
+++ scripts.js
@@ -8,25 +8,25 @@ function Idea(id, title, body, rating, complete) {
 
 function prependCard(i) {
   $('#card-box').prepend(
-    `<div class='idea-card ${i.complete}' id="${i.id}">
-      <div class='title-line'>
+    `<article class='idea-card ${i.complete}' id="${i.id}">
+      <section class='title-line'>
         <div id='line-1'>
           <h2 class='title-edit' contenteditable>${i.title}</h2>
+        </div>
         <div class="status-btn-box">
           <button id='complete-btn' class="status-btn" type="button" name="complete button"></button>
           <button id='delete-btn' class="status-btn" type="button" name="delete button"></button>
         </div>
+        <div>
+          <p id='line-2' contenteditable>${i.body}</p>
         </div>
-        <p id='line-2' contenteditable>${i.body}</p>
-      </div>
-      <div id='line-3'>
-        <button id='upvote-btn'>
-        </button>
-        <button id='downvote-btn'>
-        </button>
+      </section>
+      <section id='line-3'>
+        <button id='upvote-btn'></button>
+        <button id='downvote-btn'></button>
         <p id='rating-line'>importance: <span id="qual">${i.rating}</span></p>
-      </div>
-    </div>`
+      </section>
+    </article>`
    )
 }
 
@@ -40,12 +40,12 @@ function printCard() {
 //save button
 $('#save-btn').on('click', function() {
   var id = $.now()
-  var ideaTitle = $('#item-title').val()
-  var ideaContent = $('#item-content').val()
+  var title = $('#item-title').val()
+  var content = $('#item-content').val()
   var rating = 'normal'
   var complete = "notComplete"
-  var newIdea = new Idea(id,ideaTitle,ideaContent,rating,complete)
-  localStorage.setItem(id, JSON.stringify(newIdea))
+  var newItem = new Idea(id,title,content,rating,complete)
+  localStorage.setItem(id, JSON.stringify(newItem))
   $('#item-title').val('')
   $('#item-content').val('')
   printCard()
@@ -55,37 +55,37 @@ $('#save-btn').on('click', function() {
 $('#card-box').on('click', '#upvote-btn', function() {
   var ratingText = $(this).siblings('#rating-line').children()
     switch(ratingText.text()) {
-      case 'critical': ratingText.text('critical'); break;
-      case 'high': ratingText.text('critical'); break;
-      case 'normal': ratingText.text('high'); break;
-      case 'low': ratingText.text('normal'); break;
-      case 'none': ratingText.text('low'); break;
+      case 'critical': ratingText.text('critical'); break
+      case 'high': ratingText.text('critical'); break
+      case 'normal': ratingText.text('high'); break
+      case 'low': ratingText.text('normal'); break
+      case 'none': ratingText.text('low'); break
     }
-  var lsItem = JSON.parse(localStorage.getItem($(this).closest('.idea-card').attr('id')))
-  lsItem.rating = ratingText.text()
-  localStorage.setItem(lsItem.id, JSON.stringify(lsItem))
+  var thisCard = JSON.parse(localStorage.getItem($(this).closest('.idea-card').attr('id')))
+  thisCard.rating = ratingText.text()
+  localStorage.setItem(thisCard.id, JSON.stringify(thisCard))
 })
 
 //down vote button
 $('#card-box').on('click', '#downvote-btn', function() {
   var ratingText = $(this).siblings('#rating-line').children()
   switch(ratingText.text()) {
-    case 'critical': ratingText.text('high'); break;
-    case 'high': ratingText.text('normal'); break;
-    case 'normal': ratingText.text('low'); break;
-    case 'low': ratingText.text('none'); break;
-    case 'none': ratingText.text('none'); break;
+    case 'critical': ratingText.text('high'); break
+    case 'high': ratingText.text('normal'); break
+    case 'normal': ratingText.text('low'); break
+    case 'low': ratingText.text('none'); break
+    case 'none': ratingText.text('none'); break
   }
-  var lsItem = JSON.parse(localStorage.getItem($(this).closest('.idea-card').attr('id')))
-  lsItem.rating = ratingText.text()
-  localStorage.setItem(lsItem.id, JSON.stringify(lsItem))
+  var thisCard = JSON.parse(localStorage.getItem($(this).closest('.idea-card').attr('id')))
+  thisCard.rating = ratingText.text()
+  localStorage.setItem(thisCard.id, JSON.stringify(thisCard))
 })
 
 //task complete button
 $('#card-box').on('click', '#complete-btn', function() {
   var thisCard = JSON.parse(localStorage.getItem($(this).closest('.idea-card').attr('id')))
   switch(thisCard.complete) {
-    case 'notComplete': thisCard.complete = 'complete'; break;
+    case 'notComplete': thisCard.complete = 'complete'; break
     case 'complete': thisCard.complete = 'notComplete'; break
   }
   localStorage.setItem(thisCard.id,JSON.stringify(thisCard))
@@ -107,9 +107,9 @@ $('#card-box').on('blur', '.title-edit', function() {
 
 //content editable body
 $('#card-box').on('blur', '#line-2', function() {
-  var parseItem = JSON.parse(localStorage.getItem($(this).closest('.idea-card').attr('id')))
-  parseItem.body = $(this).text()
-  localStorage.setItem(parseItem.id, JSON.stringify(parseItem))
+  var thisCard = JSON.parse(localStorage.getItem($(this).closest('.idea-card').attr('id')))
+  thisCard.body = $(this).text()
+  localStorage.setItem(thisCard.id, JSON.stringify(thisCard))
 })
 
 //search function
